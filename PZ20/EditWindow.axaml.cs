3d5d7f0169f90270f31743b049fe24a08c829f36
lonagraf <lonagraf@mail.ru>
2023-10-09 23:57:02 +0700

    добавление и редактирование данных, иконки для каждого окна

---
+++ PZ20/EditWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PZ20;
+
+public partial class EditWindow : Window
+{
+    public EditWindow()
+    {
+        InitializeComponent();
+    }
+}
